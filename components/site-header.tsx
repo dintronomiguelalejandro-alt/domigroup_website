8d91d5ff15379948c61efe065ea3e0974352394f
dintronomiguelalejandro-alt <dintrono.miguelalejandro@gmail.com>
2026-08-16 13:08:27 +0200

    Add more vertical padding to the navbar

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -59,7 +59,7 @@ export function SiteHeader() {
           : "border-transparent bg-transparent"
       )}
     >
-      <div className="mx-auto flex max-w-6xl items-center justify-between px-10 py-4">
+      <div className="mx-auto flex max-w-6xl items-center justify-between px-10 py-6">
         <Link
           href="/"
           onClick={(e) => {
